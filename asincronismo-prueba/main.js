b28063981a30e29a0e0281ed9a33a779c3a11cc4
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-01-18 15:17:05 -0600

    Promises array

--- asincronismo-prueba/main.js
+++ asincronismo-prueba/main.js
@@ -20,6 +20,14 @@ function onError(id) {
 }
 
 
+var ids = [1, 2, 3, 4, 5, 6, 7]
+var promesas = ids.map(id => obtenerPersonaje(id))
+Promise
+    .all(promesas)
+    .then(personajes => console.log(personajes))
+    .catch(onError)
+
+
 //      Multiples promises
 // obtenerPersonaje(1)
 //     .then(persona => {
