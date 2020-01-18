afe48c9ea04fbe758edf72915c3ca3da13e94e47
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-01-18 15:16:27 -0600

    Promises

---
+++ asincronismo-prueba/main.js
@@ -0,0 +1,52 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = { crossDomain: true }
+
+
+function obtenerPersonaje(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+            .get(url, opts, data => {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+
+    })
+}
+
+function onError(id) {
+    console.log(`Error: no se pudo recuperar el personaje ${id}`)
+}
+
+
+//      Multiples promises
+// obtenerPersonaje(1)
+//     .then(persona => {
+//         console.log(`El personaje 1 es ${persona.name}`)
+//         return obtenerPersonaje(2)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 2 es ${persona.name}`)
+//         return obtenerPersonaje(3)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 3 es ${persona.name}`)
+//         return obtenerPersonaje(4)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 4 es ${persona.name}`)
+//         return obtenerPersonaje(5)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 5 es ${persona.name}`)
+//         return obtenerPersonaje(6)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 6 es ${persona.name}`)
+//         return obtenerPersonaje(7)
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 7 es ${persona.name}`)
+//     })
+//     .catch(onError)
